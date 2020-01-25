8f121456fc4a60d8b3a07f44be41985044554924
Ketan Gondaliya <gondaliya900@gmail.com>
2020-01-25 22:18:41 +0530

    first comit

---
+++ index.js
@@ -0,0 +1,5 @@
+var path = require('path');
+var word2html = require('word-to-html');
+//Word document's absolute path
+var absPath = path.join(__dirname,'test.docx');
+word2html(absPath,{tdVerticalAlign:'top'})
